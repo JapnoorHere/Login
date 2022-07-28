e61bc0ceecf1ffc0d8575f6d4894217f6199cbf1
Japnoor2104 <sjapnoor11@gmail.com>
2022-07-28 14:43:17 +0530

    change

--- app/src/main/java/com/japnoor/login/MainActivity.kt
+++ app/src/main/java/com/japnoor/login/MainActivity.kt
@@ -8,68 +8,58 @@ import androidx.core.widget.doOnTextChanged
 import com.japnoor.login.SignUpActivity
 import com.japnoor.login.ForgotPasswordActivity
 import com.japnoor.login.R
+import com.japnoor.login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var tvLOGIN : TextView
-    lateinit var etEmail : EditText
-    lateinit var etPassword : EditText
-    lateinit var btnLogin : Button
-    lateinit var btnsignUp : Button
-    lateinit var tvForgotPassword : TextView
+   lateinit var binding : ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvLOGIN=findViewById(R.id.tvLOGIN)
-        etEmail=findViewById(R.id.etEmail)
-        etPassword=findViewById(R.id.etPassword)
-        btnLogin=findViewById(R.id.btnLogin)
-        btnsignUp=findViewById(R.id.btnsignUp)
-        tvForgotPassword=findViewById(R.id.tvForgotPassword)
 
-        btnLogin.setOnClickListener {
-            var email=etEmail.text.toString()
-            var password=etPassword.text.toString()
+        binding.btnLogin.setOnClickListener {
+            var email=binding.etEmail.text.toString()
+            var password=binding.etPassword.text.toString()
             if(email.isNullOrEmpty()){
-                etEmail.error=resources.getString(R.string.please_enter_email)
-                etEmail.requestFocus()
+                binding.etEmail.error=resources.getString(R.string.please_enter_email)
+                binding.etEmail.requestFocus()
             }
             else if(password.isNullOrEmpty()){
-                etPassword.error=resources.getString(R.string.please_enter_password)
-                etPassword.requestFocus()
+                binding.etPassword.error=resources.getString(R.string.please_enter_password)
+                binding. etPassword.requestFocus()
             }
             else if(password.length<6){
-                etPassword.error=resources.getString(R.string.password_must_be_at_least_6_characters)
-                etPassword.requestFocus()
+                binding.etPassword.error=resources.getString(R.string.password_must_be_at_least_6_characters)
+                binding.etPassword.requestFocus()
             }
             else{
                 Toast.makeText(this,resources.getString(R.string.login_successful),Toast.LENGTH_LONG).show()
             }
         }
 
-        etPassword.doOnTextChanged { text, start, before, count ->
+        binding.etPassword.doOnTextChanged { text, start, before, count ->
 
             if((text?.length ?:0) <6)
             {
-                etPassword.error=resources.getString(R.string.password_must_be_at_least_6_characters)
+                binding.etPassword.error=resources.getString(R.string.password_must_be_at_least_6_characters)
             }
             else{
-                etPassword.error=null
+                binding.etPassword.error=null
             }
 
         }
 
-        btnsignUp.setOnClickListener {
+        binding.btnsignUp.setOnClickListener {
             var intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
-        tvForgotPassword.setOnClickListener{
+        binding.tvForgotPassword.setOnClickListener{
             var intent=Intent(this, ForgotPasswordActivity::class.java)
-            intent.putExtra("email",etEmail.text.toString())
+            intent.putExtra("email",binding.etEmail.text.toString())
             startActivity(intent)
         }
     }
